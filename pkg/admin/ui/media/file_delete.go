114af19bcacf997ce5e08fe14a30d3a3288dfaab
Marvin Blum <marvin.blum@emvi.com>
2025-05-23 15:33:38 +0200

    Delete multiple files.

--- pkg/admin/ui/media/file_delete.go
+++ pkg/admin/ui/media/file_delete.go
@@ -13,22 +13,37 @@ import (
 // DeleteFile deletes a file.
 func DeleteFile(w http.ResponseWriter, r *http.Request) {
 	path := strings.TrimSpace(r.URL.Query().Get("path"))
-	name := strings.TrimSpace(r.URL.Query().Get("name"))
-	fullPath := filepath.Join(getDirectoryPath(path), name)
+	names := r.URL.Query()["name[]"]
 
-	if path == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	} else if _, err := os.Stat(fullPath); os.IsNotExist(err) {
+	if len(names) == 0 {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
-	if r.Method == http.MethodDelete {
-		if err := os.Remove(fullPath); err != nil {
-			slog.Error("Error while deleting file", "error", err)
-			w.WriteHeader(http.StatusInternalServerError)
+	files := make([]string, 0, len(names))
+
+	for i, name := range names {
+		names[i] = strings.TrimSpace(name)
+		fullPath := filepath.Join(getDirectoryPath(path), names[i])
+
+		if path == "" {
+			w.WriteHeader(http.StatusBadRequest)
 			return
+		} else if _, err := os.Stat(fullPath); os.IsNotExist(err) {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		files = append(files, fullPath)
+	}
+
+	if r.Method == http.MethodDelete {
+		for _, file := range files {
+			if err := os.Remove(file); err != nil {
+				slog.Error("Error while deleting file", "error", err)
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
 		}
 
 		tpl.Get().Execute(w, "media-files.html", struct {
@@ -44,7 +59,7 @@ func DeleteFile(w http.ResponseWriter, r *http.Request) {
 	tpl.Get().Execute(w, "media-file-delete.html", struct {
 		WindowOptions ui.WindowOptions
 		Path          string
-		Name          string
+		Name          []string
 	}{
 		WindowOptions: ui.WindowOptions{
 			ID:         "shifu-media-file-delete",
@@ -53,6 +68,6 @@ func DeleteFile(w http.ResponseWriter, r *http.Request) {
 			Overlay:    true,
 		},
 		Path: path,
-		Name: name,
+		Name: names,
 	})
 }
